07b1622775b3faee39cede217a2902d827c167bd
Marco Del Giudice <m.delgiudice@outlook.it>
2021-09-10 22:22:05 +0200

    Initial project setup

---
+++ ScrollStackKit/ScrollStackKit.h
@@ -0,0 +1,18 @@
+//
+//  ScrollStackKit.h
+//  ScrollStackKit
+//
+//  Created by Marco Del Giudice on 10/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScrollStackKit.
+FOUNDATION_EXPORT double ScrollStackKitVersionNumber;
+
+//! Project version string for ScrollStackKit.
+FOUNDATION_EXPORT const unsigned char ScrollStackKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollStackKit/PublicHeader.h>
+
+
---
+++ ScrollStackKitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollStackKitDemo
+//
+//  Created by Marco Del Giudice on 10/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
